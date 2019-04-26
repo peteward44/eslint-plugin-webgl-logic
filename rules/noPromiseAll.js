4a8d18e22ec2000856aa130eacc01cefb4e07057
ramalhovfc <ramalho.vfc@gmail.com>
2019-04-26 21:47:56 +0100

    Code style fix

--- rules/noPromiseAll.js
+++ rules/noPromiseAll.js
@@ -2,19 +2,19 @@
 
 
 module.exports = function( context ) {
-    return {
-        "MemberExpression": ( node ) => {
-            if ( node.object.name === "Promise" && node.property.name === "all" ) {
-                context.report( { node, message: "Promise.all is forbidden" } );
-            }
-        }
-    };
+	return {
+		"MemberExpression": ( node ) => {
+			if ( node.object.name === "Promise" && node.property.name === "all" ) {
+				context.report( { node, message: "Promise.all is forbidden" } );
+			}
+		}
+	};
 };
 
 module.exports.schema = [
-    {
-        "type": "object",
-        "properties": {},
-        "additionalProperties": false
-    }
+	{
+		"type": "object",
+		"properties": {},
+		"additionalProperties": false
+	}
 ];
--- rules/noPromiseRace.js
+++ rules/noPromiseRace.js
@@ -2,19 +2,19 @@
 
 
 module.exports = function( context ) {
-    return {
-        "MemberExpression": ( node ) => {
-            if ( node.object.name === "Promise" && node.property.name === "race" ) {
-                context.report( { node, message: "Promise.race is forbidden" } );
-            }
-        }
-    };
+	return {
+		"MemberExpression": ( node ) => {
+			if ( node.object.name === "Promise" && node.property.name === "race" ) {
+				context.report( { node, message: "Promise.race is forbidden" } );
+			}
+		}
+	};
 };
 
 module.exports.schema = [
-    {
-        "type": "object",
-        "properties": {},
-        "additionalProperties": false
-    }
+	{
+		"type": "object",
+		"properties": {},
+		"additionalProperties": false
+	}
 ];
